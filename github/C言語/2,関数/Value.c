0b64a35b0fa0066ba238ae2b577cc6666290c555
TashironD <daikiuchiha3@gmail.com>
2022-08-30 20:55:26 +0900

    計算

--- github/C言語/2,関数/Value.c
+++ github/C言語/2,関数/Value.c
@@ -3,6 +3,25 @@
 int main(void){
     int Value;
     Value=10;
-    printf("%d",Value);
+    printf("%d\n",Value);
+
+    int apple;
+    int orange=10;
+    apple=5;
+    apple=apple+5;
+    orange+=5;
+    orange-=5;
+    orange*=5;
+    orange/=5;
+    printf("%d\n",apple+orange);
+    apple ++ ;
+    printf("%d\n",apple);
+    apple ++ ;
+    printf("%d\n",apple);
+    apple ++ ;
+    printf("%d\n",apple);
+    apple -- ;
+    printf("%d\n",apple);
     return 0;
 }
+
