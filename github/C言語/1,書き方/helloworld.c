902be00c54f5be4f467b62909f80aaf58e1a1957
TashironD <daikiuchiha3@gmail.com>
2022-08-30 16:23:34 +0900

    1,printf

---
+++ github/C言語/1,書き方/helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello\nWorld\n");
+    printf("%d円\n",100);
+
+    printf("%d+",100);
+    printf("%d=",200);
+    printf("%d\n",300);
+    printf("%d+%d=%d\n",100,200,300);
+    printf("合計で%d円です。",300);
+    return 0;
+}
