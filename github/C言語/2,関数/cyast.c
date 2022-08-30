6e208cacf9a96508b2839d077738144b54349019
TashironD <daikiuchiha3@gmail.com>
2022-08-30 21:16:21 +0900

    ２、関数、scanf

---
+++ github/C言語/2,関数/cyast.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("%f\n",3.14*3*3);
+
+    printf("%d\n",(int)3.14);
+
+    printf("%d\n",(int) (3.14*3*3));
+    return 0;
+}
---
+++ github/C言語/2,関数/scanf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+    double suuchi;
+    scanf("%lf",&suuchi);
+    printf("入力された値は%lfです。",suuchi);
+
+    return 0;
+}
