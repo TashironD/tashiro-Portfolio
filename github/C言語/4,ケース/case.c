db86f3d61c6badb19fbbb1cc822c6897b43a87d2
TashironD <daikiuchiha3@gmail.com>
2022-08-31 19:57:44 +0900

    case

---
+++ github/C言語/4,ケース/case.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void){
+
+    int no;
+    scanf("%d",&no);
+    switch (no){
+    case 1:
+        printf("ハリー");
+        break;
+    case 2:
+        printf("ロン");
+        break;
+    case 3:
+        printf("ハーマイオニー");
+        break;
+    case 4:
+        printf("マルフォイ");
+        break;
+    default:
+        printf("1~4のいずれかの数字を入力してください");
+        break;
+    }
+
+    return 0;
+}
