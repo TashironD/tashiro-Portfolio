df777ace5a2c8574c17252eb96eb1b131961b00e
TashironD <daikiuchiha3@gmail.com>
2022-08-31 10:15:40 +0900

    block

---
+++ github/C言語/3,条件分岐/block.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+
+    int suuchi;
+    scanf("%d",&suuchi);
+    if (suuchi == 10) {
+        printf("10と同じです。");
+        printf("正しく入力されました。");
+    }
+    return 0;
+}
